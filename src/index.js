a72a5630db5aded87fad49b1c2670324be4baec1
millenniumSonule <anshumansonuley@gmail.com>
2024-04-03 17:15:50 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
+import './App.css'
+import Contact from './screens/Contact.jsx';
+import Home from './screens/Home.jsx';
+import Layout from './screens/Layout.jsx';
+import NoPage from './screens/NoPage.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Layout />}>
+          <Route index element={<Home/>} />
+          <Route path="contact" element={<Contact/>} />
+          <Route path="*" element={<NoPage/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
