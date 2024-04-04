8b7654f6b18e7c73fefeb3a61534b4c30e5ca251
millenniumSonule <anshumansonuley@gmail.com>
2024-04-05 04:52:02 +0530

    .

--- src/screens/Footer.jsx
+++ src/screens/Footer.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import './assets/Footer.css'
+import logo from './assets/TastebiteLogo.png'; // Importing the logo image
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+
+      <div className="logo-discription">
+        <div className="logo">
+
+          <img src={logo} alt="Tastebite Logo" />
+        
+
+        </div>
+        <p className='disc-footer'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.Voluptate velit iste inventore distinctio voluptatum illo reprehenderit suscipit dolorem cupiditate, expedita asperiores rerum, at dolorum! Voluptates eveniet reprehenderit mollitia eligendi dicta.
+        </p>
+      </div>
+
+      <div className="links-colms">
+        <div className="tastebite-col">
+            <ul>
+              <li>Tastebite</li>
+              <li>About us </li>
+              <li>Careers</li>
+              <li>Feedback</li>
+            </ul>
+            <ul>
+              <li>Tastebite</li>
+              <li>About us </li>
+              <li>Careers</li>
+              <li>Feedback</li>
+            </ul>
+            <ul>
+              <li>Tastebite</li>
+              <li>About us </li>
+              <li>Careers</li>
+              <li>Feedback</li>
+            </ul>
+        </div>
+
+      </div>
+    
+    
+    </div>
   )
 }
 
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import NavBar from './NavBar'
+import Footer from './Footer'
 const Home = () => {
   return (
       <div className="container-homepage">
         <NavBar/>
         <h1>Homepage</h1>
+
+        <Footer/>
       </div>
   )
 }
