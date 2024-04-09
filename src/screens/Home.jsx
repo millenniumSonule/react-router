5209dfd893e35968d7fd0c1c7a13ff31fff5bd3c
millenniumSonule <anshumansonuley@gmail.com>
2024-04-09 12:33:05 +0530

    .

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -15,7 +15,6 @@ const Home = () => {
   const [popularCategory, setPopularCategory] = useState([]);
 
   const [randomMeals, setRandomMeals] = useState([]);
-  const [loading, setLoading] = useState(true); // Add loading state
 
   const fetchRandomMeals = async () => {
     try {
@@ -29,7 +28,6 @@ const Home = () => {
         }
       }
       setRandomMeals(uniqueMeals);
-      setLoading(false); // Set loading to false after data is fetched
     } catch (error) {
       console.error('Error fetching random meals:', error);
     }
@@ -57,22 +55,22 @@ const Home = () => {
           <div className="carousel-item active a-image">
             <img src={aImage} className="d-block w-100 overlay" alt="..." />
             <div className="carousel-caption">
-              <h5>Image 1</h5>
-              <p>Description for Image 1</p>
+              <h5>Get Meals Info</h5>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium?</p>
             </div>
           </div>
           <div className="carousel-item a-image">
-            <img src={bImage} className="d-block w-100 overlay" alt="..." />
+            <img src={dImage} className="d-block w-100 overlay" alt="..." />
             <div className="carousel-caption">
-              <h5>Image 2</h5>
-              <p>Description for Image 2</p>
+              <h5>Lorem ipsum dolor sit.</h5>
+              <p>Lorem, ipsum dolor. Lorem ipsum dolor sit amet consectetur.</p>
             </div>
           </div>
           <div className="carousel-item a-image">
-            <img src={cImage} className="d-block w-100 overlay" alt="..." />
+            <img src={eImage} className="d-block w-100 overlay" alt="..." />
             <div className="carousel-caption">
-              <h5>Image 3</h5>
-              <p>Description for Image 3</p>
+              <h5>Lorem, ipsum.</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
             </div>
           </div>
         </div>
@@ -107,7 +105,7 @@ const Home = () => {
 
 
       <div class="supar-delicious">
-  <h2>Supar Delicious</h2>
+  <h2>Trending Now</h2>
   <div class="random-meals d-flex">
     {randomMeals.map((meal, index) => (
       <div class="random-meal-card" key={index}>
@@ -117,6 +115,18 @@ const Home = () => {
     ))}
   </div>
 </div>
+
+      <div className="join-email-form">
+          <h1>Deliciousness <br/>to your inbox </h1>
+          <h4>Enjoy Weekly Handpick Recipes and</h4>
+          <h4>recommendations</h4>
+          <div className="email-box">
+          <div className="email-input-box">
+                <input type="text" placeholder="Email address" />
+                <button>Join</button>
+            </div>
+          </div>
+      </div>
 
       <Footer />
     </div>
