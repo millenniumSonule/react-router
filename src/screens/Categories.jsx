1ed83e1b5a25928acc1975b3869fa4a4a28c3b93
millenniumSonule <anshumansonuley@gmail.com>
2024-04-10 07:56:45 +0530

    .

--- src/screens/Categories.jsx
+++ src/screens/Categories.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 import Footer from './Footer'
-
+import './Categories.css'
 const Categories = () => {
+  const [searchResult, setSearchResult] = useState([]);
+  const [toSearch,setToSearch] = useState('');
+
+  const searchResultApi = ()=> {
+      alert(toSearch)
+  }
+
   return (
     <div className="container-homepage">
       <NavBar />
-      <h1>categories</h1>
-      <h1>categories</h1>
-      <h1>categories</h1>
-      <h1>categories</h1>
-      <h1 >categories</h1>
+      <h2 className='center-text'>Search for the food Ingradient wise</h2>
+      <div className="r-search">
+      <input
+        value={toSearch}
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => setToSearch(event.target.value)} 
+      />
+        <button onClick={searchResultApi}>Search</button>
+      </div>
 
-      <Footer/>
+      {searchResult.length > 0 ?
+        <div>
+          <p>a</p>
+        </div> : <div style={{height : '100vh'}}>
+          <p>b</p>
+        </div>
+     }
+
+
+      <Footer />
     </div>
   )
 }
